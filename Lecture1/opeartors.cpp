97326a5bbae4164ec333e19551670b686a018c49
Mosina916 <mosinaashraf43@gmail.com>
2021-11-11 21:54:03 +0530

    Basics of c++

---
+++ Lecture1/opeartors.cpp
@@ -0,0 +1,163 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	// int a,b;
+	// cin>>a>>b;//5//7
+
+	// cout<<a+b<<endl;//5+7-->12
+
+	// cout<<a-b<<endl;//5-7-->-2
+
+	// cout<<a*b<<endl;//5*7-->35*-->multiplication
+
+
+	// cout<<a/b<<endl;//5/7-->int/int-->0.71---
+
+
+
+	// int g=80+40;//=  assignment
+
+	// cout<<g<<endl;
+
+
+
+	// syntax
+
+	// if(conditions){//true tou ander jayega curly braces k
+
+
+	// }
+
+	// int cgpa=8;
+
+
+	// if hi hai bus
+
+
+	// if(cgpa==10){//kya 10 equal to 10 k hai -->true
+	// 	cout<<"my dad gifted me a car"<<endl;
+
+
+	// }
+
+
+
+	// if else
+
+	// agar if chala tou aage kabhi else chalega hi nahi
+
+	// if(cgpa==10){//true
+	// 	cout<<"my dad gifted me a car"<<endl;
+
+	// }
+	// else{
+	// 	cout<<"do hardwork "<<endl;
+	// }
+
+
+
+	// if elseif else
+
+
+	// ==-->true false
+
+
+	// if(cgpa==10){
+	// 	cout<<"my dad gifted me a car"<<endl;
+
+	// }
+	// else if(cgpa==9){
+	// 	cout<<"my dad will gift  me a PHONE"<<endl;
+
+	// }
+	// else if(cgpa==8){
+	// 	cout<<"my dad will gifted  me an earphone"<<endl;
+
+	// }
+	// else{
+	// 	cout<<"do hardwork "<<endl;
+
+	// }
+
+
+
+	// char ch='*';
+	// if(ch=='*'){
+	// 	cout<<"coding blocks";
+	// }
+
+	// else if(ch=='*'){
+	// 	cout<<"i am a student";
+	// }
+	// else{
+	// 	cout<<"invalid";
+	// }
+
+	// float cgpa=8;
+
+	// if(cgpa>=9 and cgpa<=10){ //kyunki and depend kar raha hai tou cgpa<=10 execute hoga
+	// 	// agar pehli hi condition false aaye tou dusri condition execute hi nahi hoti
+	// 	cout<<"my dad gifted me a car"<<endl;
+
+	
+
+	// }
+
+	// else{
+	// 	cout<<"work hard"<<endl;
+
+	// }
+
+
+	// int cgpa=9.8;
+
+	// cout<<cgpa<<endl;
+	// // or ||
+
+	// if(cgpa>=9 || cgpa<=10){ //kyunki and depend kar raha hai tou cgpa<=10 execute hoga
+	// 	// agar pehli hi condition false aaye tou dusri condition execute hi nahi hoti
+	// 	cout<<"my dad gifted me a car"<<endl;
+
+	
+
+	// }
+
+	// else{
+	// 	cout<<"work hard"<<endl;
+
+	// }
+
+
+
+
+if(0){//non zero value-->-ve or +ive -->true  0-->false
+cout<<"nsdbf"<<endl;
+
+} 
+else{
+	cout<<"qwerty"<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;//terminate
+}
---
+++ Lecture1/test.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+using namespace std;
+int main(){
+	// string 
+
+	// cout console output
+	// cout<<"coding blocks"<<endl;
+	// cout<<'T'<<endl;
+
+	// cout<<65346<<endl;
+
+	// cout<<"coding blocks"<<endl<<'T'<<endl<<65346<<endl;
+
+	// datatype nameofbucket;
+
+	int age;//isme garbage jayega //decleartion
+
+	cout<<age<<endl;//age--->value inside the container//garbage value
+
+	age=86;//assignment
+
+
+	
+	cout<<0<<age<<endl;//50
+
+
+
+
+
+
+	// char---
+	// '\0'--null
+
+	char ch='A';//jab container banaya saath mai hi value de dii //initilization
+
+	cout<<ch<<endl;
+
+
+	int markss1,markss2;
+	cin>>markss1>>markss2;//60
+
+
+	cout<<markss1<<endl<<markss2<<endl;//60
+
+
+	// int markss2;
+	// cin>>markss2;//80
+
+
+	// cout<<markss2<<endl;//80
+
+
+	char ch1='a',ch2='e',ch3='i',ch4='o',ch5='u';
+
+	cout<<ch1<<' '<<ch2<<" "<<ch3<<" "<<ch4<<" "<<ch5<<" "<<endl;
+
+
+
+	int cin;
+
+
+	// int 87nvfdns;
+	int _gfrwerhdfhsdf;
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
